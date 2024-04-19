6f93ea7b024bedec60827e9971eb614bd254253a
zumzum <kazonima@gmail.com>
2024-04-19 12:41:26 +0700

    routing

--- app/core/App.php
+++ app/core/App.php
@@ -4,6 +4,16 @@ class App
 {
     public function __construct()
     {
-        echo 'OK';
+        $url=$this->parseUrl();
+        var_dump($url);
+    }
+
+    public function parseUrl(){
+        if(isset($_GET['url'])){
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode("/",$url);
+            return $url;
+        }
     }
 }
