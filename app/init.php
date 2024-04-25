7e7807ca2698e34ac8c859dbcbd9902f6087cd6e
zumzum <kazonima@gmail.com>
2024-04-26 00:12:38 +0700

    learn asset

--- app/init.php
+++ app/init.php
@@ -3,3 +3,5 @@
 require_once 'core/App.php';
 
 require_once 'core/Controller.php';
+
+require_once 'core/Constants.php';
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,6 @@
-<h1>ini semua tentang aku</h1>
-<p>Nama saya <?= $data['nama'] ?>, perkerjaan saya <?= $data['pekerjaan'] ?>, umur saya <?= $data['umur'] ?> tahun</p>
+<div class="container">
+    <h1 class='mt-4'>ini semua tentang aku</h1>
+    <img src="<?= BASEURL; ?>/img/kamar.jpg" alt="kamr" width="200" class="rounded-circle shadow">
+    <p>Nama saya <?= $data['nama'] ?>, perkerjaan saya <?= $data['pekerjaan'] ?>, umur saya <?= $data['umur'] ?> tahun
+    </p>
+</div>
