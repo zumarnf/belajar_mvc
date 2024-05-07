c142aa4ba0e67a7fc40f2edb714d002a73d387e7
zumzum <kazonima@gmail.com>
2024-05-07 15:21:05 +0700

    delete_data

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -32,5 +32,20 @@ class Mahasiswa extends Controller
             header('Location: ' . BASEURL . '/mahasiswa');
             exit;
         }
+    
+    }
+
+    public function hapus($id){
+        if($this->model('Mahasiswa_model')->hapusDataMahasiswa($id) > 0){
+            Flasher::setFlash('berhasil','dihapus','success');
+
+            header('Location: ' . BASEURL . '/mahasiswa');
+            exit;
+        }else{
+            Flasher::setFlash('gagal','dihapus','danger');
+
+            header('Location: ' . BASEURL . '/mahasiswa');
+            exit;
+        }
     }
 }
